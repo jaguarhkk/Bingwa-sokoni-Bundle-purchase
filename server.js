2fcea1dd51ba54bc3baf90c9b53724dcc7aa671b
jaguarhkk <abekorir3470@gmail.com>
2025-06-30 22:45:04 +0300

    server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: 'curious-raindrop-5834c3.netlify.app'
+  origin: 'https://curious-raindrop-5834c3.netlify.app'
 }));
 app.use(express.json());
 
