a4432760db019def1ec2d9244403f53e1d672e2e
jaguarhkk <abekorir3470@gmail.com>
2025-06-30 22:43:37 +0300

    server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: 'https://vocal-gumdrop-cd34f4.netlify.app'
+  origin: 'curious-raindrop-5834c3.netlify.app'
 }));
 app.use(express.json());
 
