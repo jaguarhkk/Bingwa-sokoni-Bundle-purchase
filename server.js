f55c3211bd8f8bf823348a0d71b83c573c485caa
jaguarhkk <abekorir3470@gmail.com>
2025-06-30 20:54:31 +0300

    server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: 'https://imarikaempowermentloans.netlify.app/offers.html'
+  origin: 'https://vocal-gumdrop-cd34f4.netlify.app'
 }));
 app.use(express.json());
 
