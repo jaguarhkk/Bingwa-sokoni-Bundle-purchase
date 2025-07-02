66ddbd1ea7799e7ece4ffb5bc4ac8e3df1ab6ec7
jaguarhkk <abekorir3470@gmail.com>
2025-07-02 20:53:00 +0300

    server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: 'https://bespoke-faloodeh-4caaf7.netlify.app'
+  origin: 'https://ornate-cendol-81e3e5.netlify.app'
 }));
 app.use(express.json());
 
