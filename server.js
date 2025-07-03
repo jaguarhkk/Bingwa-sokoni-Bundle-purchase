21c950c8184f52c5ce9ea52f61e9bff0134ee319
jaguarhkk <abekorir3470@gmail.com>
2025-07-03 13:43:27 +0300

    server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: 'https://musical-nougat-7d6033.netlify.app'
+  origin: ''https://imarikadaily.netlify.app
 }));
 app.use(express.json());
 
