7ec016abebc63c7b2d9280ec8e40af2711c3e1af
jaguarhkk <abekorir3470@gmail.com>
2025-07-11 09:24:09 +0300

    server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: 'https://imarikaloanskenya.vercel.app'
+  origin: 'https://imarikacashloan.netlify.app/'
 }));
 app.use(express.json());
 
